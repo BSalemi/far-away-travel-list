9c53dc09c238917110196d20be876e4f61e7e9a7
BSalemi <briandsalemi@gmail.com>
2024-11-30 10:41:13 -0500

    added state and controlled elements for description and an event handler of onChange for the text input

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: true },
@@ -19,15 +21,30 @@ function Logo() {
 }
 
 function Form() {
+  const [description, setDescription] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
   return (
-    <div className="add-form">
+    <div className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your 😍 trip?</h3>
       <select name="quantity" id="quantity">
-        {Array.from({length:20}, (_, i) => i + 1).map((num) => {
-          return <option>{num}</option>;
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => {
+          return (
+            <option value={num} key={num}>
+              {num}
+            </option>
+          );
         })}
       </select>
-      <input label="item" type="text" placeholder="Item..."></input>
+      <input
+        label="item"
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
       <button>Add</button>
     </div>
   );
@@ -37,7 +54,7 @@ function PackingList() {
   return (
     <div className="list">
       <ul>
-        {initialItems.map((item) => (
+        {initialItems.map((item, key) => (
           <Item item={item} />
         ))}
       </ul>
