d784babac52e449c9a8b3f018087429eaa416184
BSalemi <briandsalemi@gmail.com>
2024-11-30 09:15:51 -0500

    created Item component and added an item array for placeholder

--- src/App.js
+++ src/App.js
@@ -1,3 +1,8 @@
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+];
+
 export default function App() {
   return (
     <div className="app">
@@ -22,7 +27,22 @@ function Form() {
 }
 
 function PackingList() {
-  return <div className="list">LIST</div>;
+  return (
+    <ul className="list">
+      {initialItems.map((item) => (
+        <Item item={item} />
+      ))}
+    </ul>
+  );
+}
+
+function Item(props) {
+  return (
+    <li key={props.item.id}>
+      <input type="checkbox"></input>
+      {props.item.description}
+    </li>
+  );
 }
 
 function Stats() {
