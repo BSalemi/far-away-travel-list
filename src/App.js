4b1aa3b7af9435bd3075dd28911068d40fa52221
BSalemi <briandsalemi@gmail.com>
2025-01-03 06:53:19 -0500

    added functionality to sort items by input order, description, and packed status

--- src/App.js
+++ src/App.js
@@ -85,10 +85,24 @@ function Form({ onAddItems }) {
 
 function PackingList({ items, onDeleteItem, onPackItem }) {
   const [sortBy, setSortBy] = useState("input");
+
+  let sortedItems;
+
+  if (sortBy === "input") sortedItems = items;
+
+  if (sortBy === "description")
+    sortedItems = items
+      .slice()
+      .sort((a, b) => a.description.localeCompare(b.description));
+
+  if (sortBy === "packed")
+    sortedItems = items
+      .slice()
+      .sort((a, b) => Number(a.packed) - Number(b.packed));
   return (
     <div className="list">
       <ul>
-        {items.map((item, key) => (
+        {sortedItems.map((item, key) => (
           <Item
             item={item}
             onDeleteItem={onDeleteItem}
