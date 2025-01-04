fee75ccdefa786eb618e2e1dab1a01a6d60e6925
BSalemi <briandsalemi@gmail.com>
2025-01-04 08:03:34 -0500

    moved Form and Logo components into their own files

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import Form from "./Form.js";
+import Logo from "./Logo.js";
 
 export default function App() {
   const [items, setItems] = useState([]);
@@ -38,56 +40,6 @@ export default function App() {
         onClearList={handleClearList}
       />
       <Stats items={items} />
-    </div>
-  );
-}
-
-function Logo() {
-  return <h1> 🏝️ Far Away 🧳</h1>;
-}
-
-function Form({ onAddItems }) {
-  const [description, setDescription] = useState("");
-  const [quantity, setQuantity] = useState(1);
-
-  function handleSubmit(e) {
-    e.preventDefault();
-    if (!description) {
-      return;
-    }
-    const newItem = { description, quantity, packed: false, id: Date.now() };
-
-    onAddItems(newItem);
-
-    setDescription("");
-    setQuantity(1);
-  }
-
-  return (
-    <div className="add-form" onSubmit={handleSubmit}>
-      <h3>What do you need for your 😍 trip?</h3>
-      <select
-        name="quantity"
-        id="quantity"
-        value={quantity}
-        onChange={(e) => setQuantity(Number(e.target.value))}
-      >
-        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => {
-          return (
-            <option value={num} key={num}>
-              {num}
-            </option>
-          );
-        })}
-      </select>
-      <input
-        label="item"
-        type="text"
-        placeholder="Item..."
-        value={description}
-        onChange={(e) => setDescription(e.target.value)}
-      />
-      <button onClick={handleSubmit}>Add</button>
     </div>
   );
 }
